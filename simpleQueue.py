83b5f0cd7af40f146f2000f5ed9a3c7465e32c91
jj <ryba271+git@google.com>
2014-10-16 11:03:33 +0200

    correct syntax

--- simpleQueue.py
+++ simpleQueue.py
@@ -5,24 +5,24 @@ class QueueManager(object):
     queues = dict()
 
     def publish(self, routing_key, message):
-        if routing_key not in queues:
+        if routing_key not in self.queues:
             print 'Error no queue for given routing key!'
             return
-        listner = queues[routing_key].popleft()
+        listner = self.queues[routing_key].popleft()
         listner(message)
-        queues[routing_key].append(listner)
+        self.queues[routing_key].append(listner)
 
     def subscribe(self, routing_key, listner):
-        if routing_key not in queues:
-            queues[routing_key] = deque()
+        if routing_key not in self.queues:
+            self.queues[routing_key] = deque()
 
-        queues[routing_key].append(listner)
+        self.queues[routing_key].append(listner)
 
     def unsubscribe(self, routing_key, listner):
-        if routing_key not in queues:
+        if routing_key not in self.queues:
             print 'Error no queue for given routing key!'
             return
-        q = queues.pop(routing_key)
+        q = self.queues.pop(routing_key)
         q.remove(listner)
         if len(q) > 0:
-            queues[routing_key] = q
+            self.queues[routing_key] = q

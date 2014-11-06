4fd3aaf7c7844c8fd97adf66ec6c18be42698d6b
Benedikt von St. Vieth <b.von.st.vieth@fz-juelich.de>
2014-11-06 16:12:41 +0100

    as long as we do not have tests and tester is used, make the worker backend configurable.

--- tester.py
+++ tester.py
@@ -1,17 +1,19 @@
+import ConfigParser
+import importlib
 import logging
 import time
 
-logging.basicConfig(format='%(asctime)s %(message)s',
-                    datefmt='%m/%d/%Y %I:%M:%S %p',
-                    level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
 
 from facade import get_all_instances, get_instance_types, create_instance
 
 from store_updater import init as updater_init
 updater_init()
 
-from worker import stop, init as worker_init
-worker_init()
+settings = ConfigParser.ConfigParser()
+settings.readfp(open('config.ini'))
+worker = importlib.import_module(settings.get('worker', 'backend'))
+worker.init()
 
 
 if __name__ == "__main__":
@@ -40,4 +42,4 @@ if __name__ == "__main__":
     instances = get_all_instances()
     for k, v in instances.iteritems():
         logging.debug('%s -- %s', k, v)
-    stop()
+    worker.stop()

1d090b8c2408a470c1253e5121d5c724ceb4ed5a
httpPrincess <ryba271+git@google.com>
2014-11-19 14:15:57 +0100

    mongo store reduced to simpler kv facility, updates fixed

--- stores/abstract_kv_store.py
+++ stores/abstract_kv_store.py
@@ -13,13 +13,19 @@ class AbstractKVStore(object):
             self.collection = self.initialize_collection()
 
     def update(self, name, value):
-        self.collection.insert({'name': name, 'value': value})
+        #self.collection.insert({'name': name, 'value': value})
+        self.collection.update(spec={'name': name},
+                               document={'name': name, 'value': value},
+                               upsert=True)
 
     def get(self, name):
-        return self.collection.find_one({'name': name})
+        element = self.collection.find_one({'name': name})
+        if element is None:
+            return None
+        return element['name'], element['value']
 
     def get_all(self):
-        return list(self.collection.find())
+        return {i['name']: i['value'] for i in self.collection.find()}
 
     def config_update(self, settings):
         for k, v in settings.iteritems():

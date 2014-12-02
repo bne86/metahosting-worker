7b654de636ef69a9aff81777468f54712e59d738
httpPrincess <ryba271+git@google.com>
2014-12-02 09:16:57 +0100

    lets do some testing

---
+++ tests/advanced.py
@@ -0,0 +1,8 @@
+import unittest
+
+
+class AdvancedTest(unittest.TestCase):
+
+    def test_last(self):
+        print 'Testing last'
+        self.assertTrue(True)
---
+++ tests/basic.py
@@ -0,0 +1,8 @@
+import unittest
+
+
+class BasicTest(unittest.TestCase):
+
+    def test_first(self):
+        print 'Testing first'
+        self.assertTrue(True)

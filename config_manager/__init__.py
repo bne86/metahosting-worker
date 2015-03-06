ee477ea69fe586d0df6226689cba40c7b639156e
BeneDicere <b.von.st.vieth@fz-juelich.de>
2015-03-06 16:42:18 +0100

    try to use environment overrides for config

--- config_manager/__init__.py
+++ config_manager/__init__.py
@@ -7,31 +7,33 @@ _CONFIG_FILE = 'config.ini'
 
 
 def get_configuration(section_name, config_file=None,
-                      variables=_VARIABLE_NAMES, get_all=True):
+                      variables=_VARIABLE_NAMES):
     if not config_file:
         config_file = _CONFIG_FILE
     var_config = ConfigParser.SafeConfigParser()
     var_config.read(variables)
     config = ConfigParser.SafeConfigParser()
     config.read(config_file)
-    if get_all:
-        items = config.items(section=section_name)
-    else:
-        try:
-            items = var_config.items(section=section_name)
-        except ConfigParser.NoSectionError:
-            logging.debug('var config file %s not found', variables)
-            return None
+
+    config_items = config.items(section=section_name)
+    try:
+        env_overrides = var_config.options(section=section_name)
+    except ConfigParser.NoSectionError:
+        logging.debug('var config file %s not found', variables)
+        env_overrides = []
 
     properties = {}
-    for name, variable in items:
-        if variable in os.environ:
-            properties[name] = os.getenv(variable)
+    for name, variable in config_items:
+        if name in env_overrides:
+            tmp = var_config.get(section=section_name, option=name)
+            if tmp in os.environ:
+                properties[name] = os.getenv(tmp)
         else:
             try:
-                properties[name] = config.get(section_name, name.lower())
+                properties[name] = variable
             except ConfigParser.NoOptionError:
                 logging.error('Unable to initialize %s for %s', name,
                               section_name)
                 return None
+    print properties
     return properties

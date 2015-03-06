5760ef62087c83d1ffe6bbfd59b7c93501fa9113
BeneDicere <b.von.st.vieth@fz-juelich.de>
2015-03-06 14:16:13 +0100

    missed some items

--- stores/ming_store.py
+++
@@ -1,9 +0,0 @@
-import ming
-from stores.abstract_kv_store import AbstractKVStore
-
-
-class MingStore(AbstractKVStore):
-    def initialize_collection(self):
-        client = ming.create_datastore(self.get_property('url')).conn
-        return client[self.get_property('database')][self.get_property(
-            'collection')]

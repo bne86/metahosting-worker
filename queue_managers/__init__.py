ca098ae81dc48f225fb7fba87b4510a8ad29463d
BeneDicere <b.von.st.vieth@fz-juelich.de>
2015-03-18 13:06:29 +0100

    missed to commit queue_managers...

--- queue_managers/__init__.py
+++ queue_managers/__init__.py
@@ -3,28 +3,34 @@ from config_manager import get_configuration
 from queue_managers.rabbit import BlockingPikaManager
 
 
-manager = None
+manager = dict()
 
 
-def get_manager(queue=None):
+def set_manager(queues=None):
     config = get_configuration('messaging') or {'host': 'localhost',
                                                 'port': 5672,
                                                 'user': 'guest',
                                                 'pass': 'guest'}
     global manager
+    for item in queues:
+        manager[item] = BlockingPikaManager(host=config['host'],
+                                            port=int(config['port']),
+                                            user=config['user'],
+                                            password=config['pass'],
+                                            queue=item)
+
+
+def get_manager(queue=None):
+    global manager
     if not manager:
-        manager = BlockingPikaManager(host=config['host'],
-                                      port=int(config['port']),
-                                      user=config['user'],
-                                      password=config['pass'],
-                                      queue=queue)
-    return manager
+        set_manager(queues=queue)
+    return manager[queue]
 
 
 def send_message(routing_key, subject, message):
     msg = copy.copy(message)
     msg['subject'] = subject
-    get_manager().publish(routing_key, msg)
+    get_manager(queue=routing_key).publish(routing_key, msg)
 
 
 def get_message_subject(message):
@@ -34,4 +40,4 @@ def get_message_subject(message):
 
 
 def subscribe(routing_key, callback):
-    get_manager().subscribe(routing_key, callback)
+    get_manager(queue=routing_key).subscribe(routing_key, callback)

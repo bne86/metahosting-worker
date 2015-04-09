d599adbd8c5feb2b2cf2ab3c90bca7dbf3db25a3
BeneDicere <b.von.st.vieth@fz-juelich.de>
2015-04-09 14:13:28 +0200

    mongo did not use the config, it used hard coded stuff and failed.

--- tests/test_mongo_store.py
+++ tests/test_mongo_store.py
@@ -1,47 +1,38 @@
+import config_manager
+import os
+import unittest
 from pymongo import MongoClient
 from stores.mongo_store import MongoStore
-
-__author__ = 'jj'
-
-import unittest
-
-host = '192.168.59.103'
-port = 27017
 
 
 class MongoStoreTest(unittest.TestCase):
     def setUp(self):
-        pass
+        config_manager._CONFIG_FILE = os.getenv('TEST_CONFIG', 'config.ini')
+        self.config = config_manager.get_configuration(
+            section_name='local_persistence')
+        self.config['database'] = 'metahosting_tests'
+        self.store = MongoStore(config=self.config)
 
     def tearDown(self):
-        MongoClient(host=host, port=port).drop_database('metahosting_tests')
-
-    def get_store(self):
-        config = dict()
-        config['host'] = host
-        config['port'] = port
-        config['database'] = 'metahosting_tests'
-        config['collection'] = 'tests'
-        return MongoStore(config=config)
+        MongoClient(host=self.config['host'],
+                    port=int(self.config['port']))\
+            .drop_database('metahosting_tests')
 
     def test_update(self):
-        store = self.get_store()
-        self.assertIsNone(store.get('name'))
-        store.update('name', 'value')
-        self.assertIsNotNone(store.get('name'))
-        store.update('name', 'new_value')
-        self.assertEqual(store.get('name'), 'new_value')
+        self.assertIsNone(self.store.get('name'))
+        self.store.update('name', 'value')
+        self.assertIsNotNone(self.store.get('name'))
+        self.store.update('name', 'new_value')
+        self.assertEqual(self.store.get('name'), 'new_value')
 
     def test_get(self):
-        store = self.get_store()
-        self.assertIsNone(store.get('foo'))
-        store.update('foo', 'bar')
-        self.assertIsNotNone(store.get('foo'))
-        self.assertEqual('bar', store.get('foo'))
+        self.assertIsNone(self.store.get('foo'))
+        self.store.update('foo', 'bar')
+        self.assertIsNotNone(self.store.get('foo'))
+        self.assertEqual('bar', self.store.get('foo'))
 
     def test_get_all(self):
-        store = self.get_store()
-        ret = store.get_all()
+        ret = self.store.get_all()
         self.assertDictEqual({}, ret)
         a = dict()
         a['foo'] = 'bar'
@@ -49,19 +40,18 @@ class MongoStoreTest(unittest.TestCase):
         a[1] = 'some'
         a['foo3'] = 21211
         for key, value in a.iteritems():
-            store.update(key, value)
+            self.store.update(key, value)
 
-        self.assertDictEqual(a, store.get_all())
+        self.assertDictEqual(a, self.store.get_all())
 
     def test_update_with_dict(self):
-        store = self.get_store()
         simple_dict = dict()
         simple_dict['a'] = {'foo': 'bar'}
-        store.update('foo', simple_dict)
+        self.store.update('foo', simple_dict)
         simple_dict['a'] = {'ooo': 'barr'}
-        ret = store.get('foo')
+        ret = self.store.get('foo')
         self.assertTrue('a' in ret)
         self.assertFalse('b' in ret)
         ret['c'] = {'foo': 'ba'}
-        ret2 = store.get('foo')
+        ret2 = self.store.get('foo')
         self.assertFalse('c' in ret2)

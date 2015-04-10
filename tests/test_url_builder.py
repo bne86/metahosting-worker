4d83b8a69d57c9c51e252c5c149c76b3b6bf6fdf
BeneDicere <b.von.st.vieth@fz-juelich.de>
2015-04-10 14:14:04 +0200

    most often... problems with config file path's

--- tests/test_url_builder.py
+++ tests/test_url_builder.py
@@ -8,7 +8,8 @@ EXIST_URL_FORMAT = 'http://localhost:8080/exist'
 
 
 def load_container(name):
-    with open(os.path.join('containers', name)) as ff:
+    json_path = os.getenv('TEST_CONTAINERS', 'tests/containers')
+    with open(os.path.join(json_path, name)) as ff:
         container = json.load(ff)
     return container
 

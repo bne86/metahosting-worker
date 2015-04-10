e7c3cd92e89815fbde564303c0a024236f27f31e
BeneDicere <b.von.st.vieth@fz-juelich.de>
2015-04-10 13:28:36 +0200

    improved PortManager and implemented tests for it.

--- tests/test_port_manager.py
+++ tests/test_port_manager.py
@@ -1,18 +1,68 @@
 from unittest import TestCase
+from workers.manager.port import PortManager
 
 
 class TestPortManager(TestCase):
     def setUp(self):
-        pass
+        config = {'ports': '1:5'}
+        self.port_manager = PortManager(config)
 
     def tearDown(self):
-        pass
+        self.port_manager = None
+
+    def test_lacking_config(self):
+        self.port_manager = PortManager({'foo': 'bar'})
+        ports = self.port_manager.acquire_ports(1)
+        self.assertIsNone(ports)
+
+    def test_wrong_config_1(self):
+        self.port_manager = PortManager({'ports': 'bar'})
+        ports = self.port_manager.acquire_ports(1)
+        self.assertIsNone(ports)
+
+    def test_wrong_config_2(self):
+        self.port_manager = PortManager({'ports': '1:asd'})
+        ports = self.port_manager.acquire_ports(1)
+        self.assertIsNone(ports)
+
+    def test_wrong_config_3(self):
+        self.port_manager = PortManager({'ports': '3:1'})
+        ports = self.port_manager.acquire_ports(1)
+        self.assertIsNone(ports)
 
     def test_acquire_ports(self):
-        self.fail()
+        ports = self.port_manager.acquire_ports(2)
+        self.assertIsNotNone(ports)
+
+    def test_acquire_no_ports(self):
+        ports = self.port_manager.acquire_ports(0)
+        self.assertIsNotNone(ports)
+
+    def test_acquire_to_many_ports(self):
+        ports = self.port_manager.acquire_ports(6)
+        self.assertIsNone(ports)
+
+    def test_enough_ports_left(self):
+        self.assertTrue(self.port_manager.enough_ports_left(5))
+
+    def test_enough_ports_left_not(self):
+        self.assertFalse(self.port_manager.enough_ports_left(6))
 
     def test_release_ports(self):
-        self.fail()
+        acquired_ports = self.port_manager.acquire_ports(2)
+        self.assertIsNotNone(acquired_ports)
+        self.port_manager.release_ports(acquired_ports)
+
+    def test_release_ports_empty(self):
+        acquired_ports = self.port_manager.acquire_ports(0)
+        self.assertIsNotNone(acquired_ports)
+        self.port_manager.release_ports(acquired_ports)
 
     def test_update_used_ports(self):
-        self.fail()
+        self.assertTrue(len(self.port_manager.used_ports) == 0)
+        self.port_manager.acquire_ports(2)
+        self.assertTrue(len(self.port_manager.used_ports) == 2)
+        self.port_manager.update_used_ports([])
+        self.assertTrue(len(self.port_manager.used_ports) == 2)
+        self.port_manager.update_used_ports([6, 7])
+        self.assertTrue(len(self.port_manager.used_ports) == 4)
--- workers/manager/port.py
+++ workers/manager/port.py
@@ -4,23 +4,32 @@ import logging
 class PortManager():
     def __init__(self, worker_conf):
         self.port_range = set()
-        if 'ports' in worker_conf:
+        if 'ports' in worker_conf and ':' in worker_conf['ports']:
             start, end = worker_conf['ports'].split(':')
-            for item in range(int(start), int(end)+1):
-                self.port_range.add(item)
+            try:
+                start = int(start)
+                end = int(end)
+                if start <= end:
+                    for item in range(start, end+1):
+                        self.port_range.add(item)
+                else:
+                    logging.error('start port must be smaller then end port')
+            except ValueError as err:
+                logging.error('Wrong port configuration given %s', err)
         self.port_range = frozenset(self.port_range)
         self.used_ports = set()
 
     def acquire_ports(self, count):
         available_ports = set(self.port_range.difference(self.used_ports))
-        acquired_ports = []
         if count <= len(available_ports):
+            acquired_ports = []
             for item in range(0, count):
                 port = available_ports.pop()
                 self.used_ports.add(port)
                 acquired_ports.append(port)
-        logging.debug("Acquired ports: %s", acquired_ports)
-        return acquired_ports
+            logging.debug("Acquired ports: %s", acquired_ports)
+            return acquired_ports
+        return None
 
     def enough_ports_left(self, count):
         if count > len(set(self.port_range.difference(self.used_ports))):
